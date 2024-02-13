3c7f493bb25cf049a504b6912a230ab587397c49
IfrahRamadurg <155607021+IfrahRamadurg@users.noreply.github.com>
2024-02-13 22:38:36 +0530

    Update TestScientificCalculator.py

--- TestScientificCalculator.py
+++ TestScientificCalculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 #testcases for scientific calculator
 import unittest
-import ScientificCalculatorMethods as cal
+from ScientificCalculatorMethods import compute_sqrt,compute_factorial,compute_log,compute_power
 
 class TestCalculator(unittest.TestCase):
 
@@ -10,7 +10,7 @@ class TestCalculator(unittest.TestCase):
             Testcase to compute square root of a given number
         """
         data=4
-        result=cal.compute_sqrt(data)
+        result=compute_sqrt(data)
         self.assertEqual(result,2)
 
     def test_compute_factorial(self):
@@ -18,7 +18,7 @@ class TestCalculator(unittest.TestCase):
             Testcase to compute factorial of a given number
         """
         data=5
-        result=cal.compute_factorial(data)
+        result=compute_factorial(data)
         self.assertEqual(result,120)
 
     def test_compute_log(self):
@@ -26,7 +26,7 @@ class TestCalculator(unittest.TestCase):
             Testcase to compute natural logarithm of a given number
         """
         data=2
-        result=cal.compute_log(data)
+        result=compute_log(data)
         self.assertEqual(result,0.69)
 
     def test_compute_power(self):
@@ -35,7 +35,7 @@ class TestCalculator(unittest.TestCase):
         """
         base_number=7
         power_number=3
-        result=cal.compute_power(base_number,power_number)
+        result=compute_power(base_number,power_number)
         self.assertEqual(result,343)
 
 if __name__=='__main__':
